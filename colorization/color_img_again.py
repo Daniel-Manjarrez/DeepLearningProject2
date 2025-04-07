3a601153235edb900b986e53a3155f98d1ec9c9c
Mahdi Ali-Raihan <65004219+asder8215@users.noreply.github.com>
2025-04-07 13:43:43 -0400

    Color_img_again change again

--- colorization/color_img_again.py
+++ colorization/color_img_again.py
@@ -18,24 +18,37 @@ class ColorizationCNN(nn.Module):
         super(ColorizationCNN, self).__init__()
         
         # Downsampling with Convolution, Batch Normalization
-        self.down1 = self.upconv_layer(1, 64, 3, 1, 1)
+        # self.down1 = self.upconv_layer(1, 64, 3, 1, 1)
+        # self.down2 = self.upconv_layer(64, 128)
+        # self.down3 = self.upconv_layer(128, 256)
+        # self.down4 = self.upconv_layer(256, 512)
+        # self.down5 = self.upconv_layer(512, 1024)
+        # self.down6 = self.upconv_layer(1024, 2048)
+
+        # Downsampling with Convolution, Batch Normalization
+        self.down1 = self.upconv_layer(1, 64)
         self.down2 = self.upconv_layer(64, 128)
         self.down3 = self.upconv_layer(128, 256)
         self.down4 = self.upconv_layer(256, 512)
-        self.down5 = self.upconv_layer(512, 1024)
-        self.down6 = self.upconv_layer(1024, 2048)
+        self.down5 = self.upconv_layer(512, 512)
+        
+        self.up5 = self.downconv_layer(512, 512)
+        self.up4 = self.downconv_layer(512, 256)
+        self.up3 = self.downconv_layer(256, 128)
+        self.up2 = self.downconv_layer(128, 64)
+        self.up1 = self.downconv_layer(64, 2)  # Output 2 channels for a* and b*
         
         # self.up5 = self.downconv_layer(512, 512)
         # self.up4 = self.downconv_layer(512, 256)
         # self.up3 = self.downconv_layer(256, 128)
         # self.up2 = self.downconv_layer(128, 64)
         # self.up1 = self.downconv_layer(64, 2)  # Output 2 channels for a* and b*
-        self.up6 = self.downconv_layer(2048, 1024)
-        self.up5 = self.downconv_layer(1024, 512)
-        self.up4 = self.downconv_layer(512, 256)
-        self.up3 = self.downconv_layer(256, 128)
-        self.up2 = self.downconv_layer(128, 64)
-        self.up1 = self.downconv_layer(64, 2, ksize=3, stride = 1, padding=1)
+        # self.up6 = self.downconv_layer(2048, 1024)
+        # self.up5 = self.downconv_layer(1024, 512)
+        # self.up4 = self.downconv_layer(512, 256)
+        # self.up3 = self.downconv_layer(256, 128)
+        # self.up2 = self.downconv_layer(128, 64)
+        # self.up1 = self.downconv_layer(64, 2, ksize=3, stride = 1, padding=1)
     
     def upconv_layer(self, in_channels, out_channels, ksize = 3, stride = 2, padding =1):
         return nn.Sequential(
@@ -44,7 +57,7 @@ class ColorizationCNN(nn.Module):
             nn.ReLU(inplace=True)
         )
     
-    def downconv_layer(self, in_channels, out_channels, ksize=4, stride=2, padding=1, is_tanh=False):
+    def downconv_layer(self, in_channels, out_channels, ksize=3, stride=2, padding=1, is_tanh=False):
         if not is_tanh:
             return nn.Sequential(
                 nn.ConvTranspose2d(in_channels, out_channels, kernel_size=ksize, stride=stride, padding=padding),
@@ -66,10 +79,10 @@ class ColorizationCNN(nn.Module):
         x = self.down3(x)
         x = self.down4(x)
         x = self.down5(x)
-        x = self.down6(x)
+        # x = self.down6(x)
 
         # Performing upsampling
-        x = self.up6(x)
+        # x = self.up6(x)
         x = self.up5(x)
         x = self.up4(x)
         x = self.up3(x)
